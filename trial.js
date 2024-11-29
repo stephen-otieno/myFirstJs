47def6bcc3484193dcec0f83054adf8efd309ef6
stephen-otieno <stephenotieno8781@gmail.com>
2024-11-29 16:59:25 +0300

    jsTesting

---
+++ trial.js
@@ -0,0 +1,14 @@
+var num1 = 20;
+var num2 = 35;
+var num1 = 23; // this line of code overrides the initial value of num1
+
+var sum = num1 + num2;
+
+var name = "Engineer";
+
+// console.log(name);
+console.log("My name is " + name + " and I am " + sum + " years old");
+let student = {}
+student.name = "stephen";
+console.log(student.name);
+// console.log(sum);
